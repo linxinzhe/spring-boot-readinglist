b87aaddd6f77197940aa945bbd7f2ba95b098077
linxinzhe <linxinzhechn@qq.com>
2018-10-07 16:52:48 +0800

    Initial commit

---
+++ src/main/java/top/linxz/java/spring/readinglist/ReadinglistApplication.java
@@ -0,0 +1,12 @@
+package top.linxz.java.spring.readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadinglistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadinglistApplication.class, args);
+    }
+}
